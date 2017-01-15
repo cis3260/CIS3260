37e4239df3a32cbe5de7d7aa66873ba205a43f89
saffarizadeh <ksaffarizadeh1@student.gsu.edu>
2017-01-15 14:30:31 -0500

    A few changes in the comments

--- Examples/src/TV.java
+++ Examples/src/TV.java
@@ -1,8 +1,8 @@
 
 public class TV {
 	String brand;
-	boolean isSmart; // boolean data-type gets the values "true" or "false"
-	boolean isLED; // if the TV is of LED type then this variable is true; otherwise is false
+	boolean isSmart; // boolean data-type accepts the values "true" or "false"
+	boolean isLED; // if the TV LED then this variable should be true; otherwise it should be false
 	boolean is4K;
 	int screenSize;
 	int numberOfHDMIInputs;
@@ -11,13 +11,13 @@ public class TV {
 	
 	void turnOn(){
 		status = true;
-		// we later learn the keyword "this" and then we will change the code to: this.status = true;
+		// later we will learn the keyword "this" and then we will change the code to: this.status = true;
 		System.out.println("TV is on");
 	}
 	
 	void turnOff(){
 		status = false;
-		// we later learn the keyword "this" and then we will change the code to: this.status = false;
+		// later we will learn the keyword "this" and then we will change the code to: this.status = false;
 		System.out.println("TV is off");
 	}
 
